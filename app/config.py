5b47eb92df90abf2b1ecb90cc724ca690d671744
Linh Pham <question+github@closedsrc.org>
2022-01-07 23:05:42 -0800

    Add a default value for pool_size of 10 if config key is missing

--- app/config.py
+++ app/config.py
@@ -10,12 +10,16 @@ API_VERSION = "2.0"
 APP_VERSION = "2.0.0-beta.4"
 
 
-def load_database_config(config_file_path: str = "config.json") -> Dict[str, Any]:
+def load_database_config(config_file_path: str = "config.json",
+                         connection_pool_size: int = 10) -> Dict[str, Any]:
     """Reads in database configuration values from a configuration
     JSON file and returns a dictionary with the values.
 
     :param config_file: Path to the configuration JSON file
     :type config_file: str, optional
+    :param connection_pool_size: Number of connections to use in
+        creating a connection pool
+    :type connection_pool_size: int, optional
     :return: Dictionary containing database configuration settings
     :rtype: Dict[str, Any]
     """
@@ -23,6 +27,9 @@ def load_database_config(config_file_path: str = "config.json") -> Dict[str, Any
         config_dict = json.load(config_file)
 
     if "database" in config_dict:
+        if "pool_size" not in config_dict["database"]:
+            config_dict["database"]["pool_size"] = connection_pool_size
+
         return config_dict["database"]
     else:
         return {}
