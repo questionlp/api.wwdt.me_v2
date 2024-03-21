150d54843d8b34cc129ee751cebcffaede963e8f
Linh Pham <questionlp@users.noreply.github.com>
2024-03-20 20:06:12 -0700

    Bump black to 24.3.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,11 @@
 # Changes
 
+## 2.8.5
+
+### Development Changes
+
+- Upgrade black from 23.12.1 to 24.3.0
+
 ## 2.8.4
 
 ### Component Changes
--- app/config.py
+++ app/config.py
@@ -10,7 +10,7 @@ from pathlib import Path
 from typing import Any
 
 API_VERSION = "2.0"
-APP_VERSION = "2.8.4"
+APP_VERSION = "2.8.5"
 
 
 def load_config(
